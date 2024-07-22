43bd0457cb815ecbafee5f2f47c1da783f95ded0
WebSpaceLab <133574117+WebSpaceLab@users.noreply.github.com>
2024-07-22 14:19:10 +0200

    feat: Adding packages to Nuxt.js.

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+    "@nuxt/eslint",
+    "nuxt-auth-utils",
+    '@pinia/nuxt',
+    "@nuxt/content",
+    "@nuxt/image",
+    '@nuxtjs/color-mode',
+  ],
+  runtimeConfig: {
+    public: {
+      clientUrl: process.env.NUXT_PUBLIC_CLIENT_URL,
+      apiUrl: process.env.NUXT_PUBLIC_API_URL,
+    },
+  },
 })
